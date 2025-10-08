8dca145df04ecceb441fa8155888cecca0f24550
George Nicanor Ivanov <ivanov6000@gmail.com>
2025-10-08 17:53:50 -0400

    Version 2.1.3. Minor edits.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,14 +29,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "da9ebb77034ca82a3940277d09a5d1ca
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "68964cc22eda8c543c0dd17bed42152c",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9af6e559812cd8cd1b61ccb42608bd4c",
+"flutter_bootstrap.js": "d6a76ff33f72bd1e8f259448c6a8d0fc",
 "icons/Icon-192.png": "5b859da8e771176934ac80a7678bf50f",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "a34232b8d6583d17029a48532464f2fe",
 "/": "a34232b8d6583d17029a48532464f2fe",
-"main.dart.js": "a4ce5e841fcde7283e2b27a61be7deea",
+"main.dart.js": "cbf6f0247a4621558b4b8c7ee2901e2c",
 "version.json": "fafc0be9c2a13ba09764c16f55ee7fa1"};
 // The application shell files that are downloaded before a service worker can
 // start.
